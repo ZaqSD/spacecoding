00d835b642fc5e4c70618f164cdff7c249a41a7f
Andreas <andreas.siaplaouras@object.ch>
2022-11-17 10:25:28 +0100

    Create homeview

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,23 @@
-import './App.css';
+import "./App.css";
 
-import {
-  Link,
-  Route,
-  BrowserRouter as Router,
-  Routes
-} from "react-router-dom";
+import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 import Home from './views/Home';
+import Home from "./views/Home";
 import React from "react";
+import { ScreenClassProvider } from "react-grid-system";
 
 function App() {
   return (
-<Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
-</Router>
+    <ScreenClassProvider>
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
+    </ScreenClassProvider>
   );
 }
 
--- frontend/src/views/Home.tsx
+++ frontend/src/views/Home.tsx
@@ -1,14 +1,22 @@
-import * as React from 'react';
+import * as React from "react";
 
-import NavBar from "../components/NavBar"
+import { Col, Container, Row } from "react-grid-system";
+
+import MessageWindow from "../components/MessageWindow";
+import NavBar from "../components/NavBar";
 
 export default function Home() {
-
-    return (
-      <>
-        <NavBar />
-        <p>Hello World</p>
-      </>
-    );
-  }
+  return (
+    <>
+      <NavBar />
+      <Container fluid>
+        <Row>
+          <Col lg={9}>Test</Col>
+          <Col lg={3}>
   
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+}
