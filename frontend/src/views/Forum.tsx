e7dea344afebf6bb138deca58274b9daf2e36e4e
Andreas <andreas.siaplaouras@object.ch>
2022-11-25 15:57:52 +0100

    Create forumpage
    forumpage can display posts now
    temp

--- frontend/src/views/Forum.tsx
+++ frontend/src/views/Forum.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 
 import { Col, Container, Row } from "react-grid-system";
 
+import ForumPost from "../components/ForumPost";
 import MessageWindow from "../components/MessageWindow";
 import NavBar from "../components/NavBar";
 
@@ -11,7 +12,48 @@ export default function Home() {
       <NavBar />
       <Container fluid>
         <Row>
-          <Col lg={9}>Test</Col>
+          <Col lg={9}>
+            <Row>
+              <Col lg={10}>
+                <h1>Forum</h1>
+              </Col>
+              <Col lg={2}>
+                <button className="button buttonUpload">Create Post</button>
+              </Col>
+            </Row>
+            <Col lg={2}>
+              <div></div>
+            </Col>
+            <Col>
+              <Row>
+                <ForumPost
+                  title="My newest Post"
+                  content="This is the newest post in a line of many posts."
+                  isLiked={true}
+                  likeNr={12}
+                  commentNr={2}
+                />
+              </Row>
+              <Row>
+                <ForumPost
+                  title="Hello World"
+                  content="This is another post."
+                  isLiked={true}
+                  likeNr={879}
+                  commentNr={0}
+                />
+              </Row>
+              <Row>
+                <ForumPost
+                  title="TestPost"
+                  content="This is a post to test new functionalities."
+                  isLiked={false}
+                  likeNr={7}
+                  commentNr={0}
+                />
+              </Row>
+            </Col>
+          </Col>
           <Col lg={3}>
             <MessageWindow />
           </Col>
